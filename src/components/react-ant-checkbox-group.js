cd7242b21dadcd8d7e4505486c3146db5a1eff4c
afeiship <1290657123@qq.com>
2018-05-22 15:45:03 +0800

    issue: event to target.value

--- src/components/react-ant-checkbox-group.js
+++ src/components/react-ant-checkbox-group.js
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import noop from 'noop';
 import objectAssign from 'object-assign';
 import { Checkbox } from 'antd';
+import 'next-return-event';
 
 export default class extends PureComponent{
   /*===properties start===*/
@@ -20,10 +21,15 @@ export default class extends PureComponent{
   };
   /*===properties end===*/
 
+  _onChange = inEvent =>{
+    const { onChange } = this.props;
+    onChange( nx.returnEventTarget( inEvent ) );
+  };
+
   render(){
-    const { className, items, template,  ...props } = this.props;
+    const { className, items, template, onChange, ...props } = this.props;
     return (
-      <Checkbox.Group {...props} className={classNames('react-ant-checkbox-group',className)}>
+      <Checkbox.Group onChange={this._onChange} className={classNames('react-ant-checkbox-group', className)}  {...props}>
       {
         (items.length > 0) && items.map((item, index)=>{
             return template(item, index) || (
